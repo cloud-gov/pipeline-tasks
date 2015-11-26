7ad773a358c3bbf8a5ffc3a6bc8e082cbc1a15ff
Adrian Webb <adrian.webb@gsa.gov>
2015-11-26 02:07:12 -0500

    Updates to the inflate shared task and script.

--- inflate.sh
+++ inflate.sh
@@ -10,22 +10,19 @@ if [ -z "$INPUT_DIR" ]; then
   echo "must specify \$INPUT_DIR" >&2
   exit 1
 fi
-#if [ -z "$OUTPUT_DIR" ]; then
-  # echo "must specify \$OUTPUT_DIR" >&2
-  # exit 1
-#fi
-
-echo "`ls -al .`"
-echo "`pwd`"
 
 #
 # Temporary file for storing compressed directory
 #
 TEMP_FILE=/tmp/inflate-`date +"%m-%d-%Y-%T"`.tar.gz
+INFLATED_DIR=inflated-dir
 
 #
 # Inflate a given directory into a new one with symbolic links dereferenced
 #
 tar -czh "$INPUT_DIR" > "$TEMP_FILE"
-tar -xzf "$TEMP_FILE" # Will be extracted to the current directory
+
+mkdir -p "$INFLATED_DIR"
+tar -xzf "$TEMP_FILE" -C "$INFLATED_DIR"
+
 rm -f "$TEMP_FILE"
