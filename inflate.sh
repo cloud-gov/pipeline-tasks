8dc6cb848fe6c6573c19e22f8be8279ebc334955
Adrian Webb <adrian.webb@gsa.gov>
2015-11-26 01:51:24 -0500

    Fixing the date and removing temp file in the inflate shared task script.

--- inflate.sh
+++ inflate.sh
@@ -18,11 +18,11 @@ fi
 #
 # Temporary file for storing compressed directory
 #
-TEMP_FILE=/tmp/inflate-`date`.tar.gz
+TEMP_FILE=/tmp/inflate-`date +"%m-%d-%Y-%T"`.tar.gz
 
 #
 # Inflate a given directory into a new one with symbolic links dereferenced
 #
 tar -czh "$INPUT_DIR" > "$TEMP_FILE"
 tar -xzf "$TEMP_FILE" # Will be extracted to the current directory
-
+rm -f "$TEMP_FILE"
