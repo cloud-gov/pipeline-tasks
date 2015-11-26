bbbf1037e07a82951404ab8c84db7c4382f6c74f
Adrian Webb <adrian.webb@gsa.gov>
2015-11-26 02:21:14 -0500

    Testing stuff in the shared inflate task and script.

--- inflate.sh
+++ inflate.sh
@@ -10,10 +10,10 @@ if [ -z "$INPUT_DIR" ]; then
   echo "must specify \$INPUT_DIR" >&2
   exit 1
 fi
-if [ -z "$OUTPUT_DIR" ]; then
-  echo "must specify \$OUTPUT_DIR" >&2
-  exit 1
-fi
+#if [ -z "$OUTPUT_DIR" ]; then
+  #  echo "must specify \$OUTPUT_DIR" >&2
+  #exit 1
+#fi
 
 #
 # Temporary file for storing compressed directory
@@ -25,7 +25,7 @@ TEMP_FILE=/tmp/inflate-`date +"%m-%d-%Y-%T"`.tar.gz
 #
 tar -czh "$INPUT_DIR" > "$TEMP_FILE"
 
-mkdir -p "$OUTPUT_DIR"
-tar -xzf "$TEMP_FILE" -C "$OUTPUT_DIR"
+mkdir -p "$INPUT_DIR/inflated"
+tar -xzf "$TEMP_FILE" -C "$INPUT_DIR/inflated"
 
 rm -f "$TEMP_FILE"
