182bede4e7421d2d68a90ecaa6a9506f3dc1499d
Adrian Webb <adrian.webb@gsa.gov>
2015-11-26 03:09:26 -0500

    Testing stuff in the shared inflate task and script.

--- inflate.sh
+++ inflate.sh
@@ -16,16 +16,11 @@ fi
 #
 NOW=`date +"%m-%d-%Y-%H-%M-%S"`
 TEMP_FILE="inflate-$NOW.tar.gz"
-OUTPUT_DIR="extract"
 
 #
 # Inflate a given directory into a new one with symbolic links dereferenced
 #
-tar -czhf "$TEMP_FILE" "$INPUT_DIR"
+tar -C "$INPUT_DIR" -czhf "$TEMP_FILE" "$INPUT_DIR"
+tar -xzf "$TEMP_FILE"
 
-mkdir -p "$OUTPUT_DIR"
-tar -xzf "$TEMP_FILE" -C "$OUTPUT_DIR"
-mv "$OUTPUT_DIR"/* .
-
-rm -Rf "$OUTPUT_DIR"
 rm -f "$TEMP_FILE"
