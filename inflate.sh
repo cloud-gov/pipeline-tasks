9cd7f30e053492530ccb1eef8d37d32383832193
Adrian Webb <adrian.webb@gsa.gov>
2015-11-26 02:35:00 -0500

    Testing stuff in the shared inflate task and script.

--- inflate.sh
+++ inflate.sh
@@ -10,10 +10,6 @@ if [ -z "$INPUT_DIR" ]; then
   echo "must specify \$INPUT_DIR" >&2
   exit 1
 fi
-#if [ -z "$OUTPUT_DIR" ]; then
-  #  echo "must specify \$OUTPUT_DIR" >&2
-  #exit 1
-#fi
 
 #
 # Temporary file for storing compressed directory
@@ -24,8 +20,5 @@ TEMP_FILE=/tmp/inflate-`date +"%m-%d-%Y-%T"`.tar.gz
 # Inflate a given directory into a new one with symbolic links dereferenced
 #
 tar -czh "$INPUT_DIR" > "$TEMP_FILE"
-
-mkdir -p "$INPUT_DIR/inflated"
-tar -xzf "$TEMP_FILE" -C "$INPUT_DIR/inflated"
-
+tar -xzf "$TEMP_FILE" -C "$INPUT_DIR"
 rm -f "$TEMP_FILE"
