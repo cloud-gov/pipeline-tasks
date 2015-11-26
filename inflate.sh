660be2b237b733194bd42c469f5e9a13747196e7
Adrian Webb <adrian.webb@gsa.gov>
2015-11-26 01:47:51 -0500

    Adding an initial inflate shared task and script.

---
+++ inflate.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# vim: set ft=sh
+
+set -e -x
+
+#
+# A little environment validation
+#
+if [ -z "$INPUT_DIR" ]; then
+  echo "must specify \$INPUT_DIR" >&2
+  exit 1
+fi
+#if [ -z "$OUTPUT_DIR" ]; then
+  # echo "must specify \$OUTPUT_DIR" >&2
+  # exit 1
+#fi
+
+#
+# Temporary file for storing compressed directory
+#
+TEMP_FILE=/tmp/inflate-`date`.tar.gz
+
+#
+# Inflate a given directory into a new one with symbolic links dereferenced
+#
+tar -czh "$INPUT_DIR" > "$TEMP_FILE"
+tar -xzf "$TEMP_FILE" # Will be extracted to the current directory
+
