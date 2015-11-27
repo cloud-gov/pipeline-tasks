6f4a415ff88ee65ddb1549a659ce5740707e7ff7
Adrian Webb <adrian.webb@gsa.gov>
2015-11-27 17:45:12 -0500

    Adding a write_file shared task and script.

---
+++ write_file.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# vim: set ft=sh
+
+set -e -x
+
+#
+# A little environment validation
+#
+if [ -z "$CONTENT" ]; then
+  echo "must specify \$CONTENT" >&2
+  exit 1
+fi
+if [ -z "$FILE_NAME" ]; then
+  echo "must specify \$FILE_NAME" >&2
+  exit 1
+fi
+
+#
+# Render content into a file
+#
+cat "$CONTENT" > "$FILE_NAME"
