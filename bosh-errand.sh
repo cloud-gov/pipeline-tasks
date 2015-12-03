8acb925e97f7cd70cd6eb628f2aad07a8ecac9c8
M. Adam Kendall <michael.kendall@gsa.gov>
2015-12-03 15:44:37 -0500

    Start the CF deploy pipeline with smoke and acceptance tests

---
+++ bosh-errand.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# vim: set ft=sh
+
+set -e
+
+#
+# A little environment validation
+#
+if [ -z "$BOSH_TARGET" ]; then
+  echo "must specify \$BOSH_TARGET" >&2
+  exit 1
+fi
+if [ -z "$BOSH_USERNAME" ]; then
+  echo "must specify \$BOSH_USERNAME" >&2
+  exit 1
+fi
+if [ -z "$BOSH_PASSWORD" ]; then
+  echo "must specify \$BOSH_PASSWORD" >&2
+  exit 1
+fi
+if [ -z "$BOSH_DEPLOYMENT_NAME" ]; then
+  echo "must specify \$BOSH_DEPLOYMENT_NAME" >&2
+  exit 1
+fi
+if [ -z "$BOSH_ERRAND" ]; then
+  echo "must specify \$BOSH_ERRAND" >&2
+  exit 1
+fi
+#
+# Run the CF smoke test errand for the appropriate deployment
+#
+bosh -n target $BOSH_TARGET
+bosh -n login $BOSH_USERNAME $BOSH_PASSWORD
+bosh -n download manifest $BOSH_DEPLOYMENT_NAME ${BOSH_DEPLOYMENT_NAME}.yml
+bosh -n deployment ${BOSH_DEPLOYMENT_NAME}.yml
+bosh -n run errand $BOSH_ERRAND
