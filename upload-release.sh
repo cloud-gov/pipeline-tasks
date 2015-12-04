64763c42773b57c73079fa79395a8b22a559e85f
M. Adam Kendall <michael.kendall@gsa.gov>
2015-12-04 15:21:47 -0500

    In order for bosh upload to skip upload of existing releases, it must be provided with a name and version. Due to this, make these part of the environment instead of args to the script

--- upload-release.sh
+++ upload-release.sh
@@ -6,10 +6,6 @@ set -e
 #
 # A little environment validation
 #
-if [ -z "$1" ]; then
-  echo "must specify release url" >&2
-  exit 1
-fi
 if [ -z "$BOSH_TARGET" ]; then
   echo "must specify \$BOSH_TARGET" >&2
   exit 1
@@ -22,10 +18,22 @@ if [ -z "$BOSH_PASSWORD" ]; then
   echo "must specify \$BOSH_PASSWORD" >&2
   exit 1
 fi
+if [ -z "$RELEASE_URL_FILE" ]; then
+  echo "must specify \$RELEASE_URL_FILE" >&2
+  exit 1
+fi
+if [ -z "$RELEASE_NAME" ]; then
+  echo "must specify \$RELEASE_NAME" >&2
+  exit 1
+fi
+if [ -z "$RELEASE_VERSION_FILE" ]; then
+  echo "must specify \$RELEASE_VERSION_FILE" >&2
+  exit 1
+fi
 
 #
 # Target BOSH
 #
 bosh -n target $BOSH_TARGET
 bosh -n login $BOSH_USERNAME $BOSH_PASSWORD
-bosh -n upload release `cat $1` --skip-if-exists
+bosh -n upload release `cat $RELEASE_URL_FILE` --name $RELEASE_NAME --version `cat $RELEASE_VERSION_FILE`
