44b080de1bfb51fc55025ec8926b422e9c3da03b
Diego Lapiduz <diego.lapiduz@gsa.gov>
2016-01-11 22:37:54 -0600

    Test CA cert in bosh errand

--- bosh-errand.sh
+++ bosh-errand.sh
@@ -26,11 +26,15 @@ if [ -z "$BOSH_ERRAND" ]; then
   echo "must specify \$BOSH_ERRAND" >&2
   exit 1
 fi
+if [ -z "$BOSH_CACERT" ]; then
+  echo "must specify \$BOSH_CACERT" >&2
+  exit 1
+fi
 #
 # Run the CF smoke test errand for the appropriate deployment
 #
-bosh -n target $BOSH_TARGET
-bosh -n login $BOSH_USERNAME $BOSH_PASSWORD
+bosh -n target $BOSH_TARGET --ca-cert $BOSH_CACERT
+echo -n \"$BOSH_USER\n$BOSH_PASSWORD\" | bosh login 1>/dev/null
 bosh -n download manifest $BOSH_DEPLOYMENT_NAME ${BOSH_DEPLOYMENT_NAME}.yml
 bosh -n deployment ${BOSH_DEPLOYMENT_NAME}.yml
 bosh -n run errand $BOSH_ERRAND
