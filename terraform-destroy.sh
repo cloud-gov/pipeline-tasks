55000ef1f1a0a2444171e3ce6a5b5bd5c0856e1a
M. Adam Kendall <michael.kendall@gsa.gov>
2016-05-19 10:27:03 -0400

    Use S3 for remote state by default

--- terraform-destroy.sh
+++ terraform-destroy.sh
@@ -3,30 +3,38 @@
 
 set -e
 
-if [ -z "$STATE_FILE" ]; then
-  echo "must specify \$STATE_FILE" >&2
+if [ -z "$STACK_NAME" ]; then
+  echo "must specify \$STACK_NAME" >&2
   exit 1
 fi
 
-if [ -z "$TF_VAR_ACCESS_KEY_ID" ]; then
-  echo "must specify \$TF_VAR_ACCESS_KEY_ID" >&2
+if [ -z "$S3_TFSTATE_BUCKET"]; then
+  echo "must specify \$S3_TFSTATE_BUCKET" >&2
   exit 1
 fi
 
-if [ -z "$TF_VAR_SECRET_ACCESS_KEY" ]; then
-  echo "must specify \$TF_VAR_SECRET_ACCESS_KEY" >&2
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+  echo "must specify \$AWS_ACCESS_KEY_ID" >&2
   exit 1
 fi
 
-if [ -z "$TF_VAR_DEFAULT_REGION" ]; then
-  echo "must specify \$TF_VAR_DEFAULT_REGION" >&2
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+  echo "must specify \$AWS_SECRET_ACCESS_KEY" >&2
   exit 1
 fi
+
+if [ -z "$AWS_DEFAULT_REGION" ]; then
+  echo "must specify \$AWS_DEFAULT_REGION" >&2
+  exit 1
+fi
+
+terraform remote config \
+  -backend=s3 \
+  -backend-config="bucket=${S3_TFSTATE_BUCKET}" \
+  -backend-config="key=${STACK_NAME}/terraform.tfstate"
 
 terraform destroy \
     -refresh=true \
-    -state=terraform-state/${STATE_FILE} \
-    -state-out=terraform-result/${STATE_FILE} \
     -force \
     terraform-templates
 
