12984cbcb5f5360c846ef27f24d3f6e48e77fb32
Joshua Carp <jm.carp@gmail.com>
2016-08-15 11:36:56 -0400

    Add script to register service brokers.

---
+++ register-service-broker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+set -u
+
+# Authenticate
+cf login -a $CF_API_URL -u $CF_USERNAME -p $CF_PASSWORD -o $CF_ORGANIZATION -s $CF_SPACE
+
+# Get service broker URL
+BROKER_URL=https://$(cf app $BROKER_NAME | grep urls: | sed 's/urls: //')
+
+# Create or update service broker
+if ! cf create-service-broker $BROKER_NAME $AUTH_USER $AUTH_PASS $BROKER_URL; then
+  cf update-service-broker $BROKER_NAME $AUTH_USER $AUTH_PASS $BROKER_URL
+fi
+
+# Enable access to service plans
+ARGS=()
+if [ -n "$SERVICE_ORGANIZATION" ]; ARGS+=("-o", "$SERVICE_ORGANIZATION"); fi
+if [ -n "$SERVICE_PLAN" ]; ARGS+=("-p", "$SERVICE_PLAN"); fi
+for SERVICE_NAME in $(echo $SERVICE_NAMES); do
+  cf enable-service-access $SERVICE_NAME "${ARGS[@]}"
+done
