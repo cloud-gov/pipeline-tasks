53fe012f0fbfe943942eb3a1c557b37287ff6592
Joshua Carp <jm.carp@gmail.com>
2016-11-03 09:33:52 -0400

    Fix typo in task config.

--- terraform-state-to-yaml.rb
+++ terraform-state-to-yaml.rb
@@ -3,9 +3,11 @@
 require 'json'
 require 'yaml'
 
-outputs=JSON.load($stdin)
-terraform_outputs = {
-  'terraform_outputs' => outputs['modules'][0]['outputs']
+outputs = JSON.load($stdin)
+
+terraform_outputs = { 'terraform_outputs' => {} }
+outputs['modules'][0]['outputs'].each {|k, v|
+  terraform_outputs['terraform_outputs'][k] = v.fetch("value")
 }
 
 puts YAML.dump(terraform_outputs)
