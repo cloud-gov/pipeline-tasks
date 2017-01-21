f694e72feb32323a055bf31de4caafa320e7da3d
Joshua Carp <jm.carp@gmail.com>
2017-01-21 13:20:36 -0500

    Log in with client credentials if passed.

--- bosh-errand.sh
+++ bosh-errand.sh
@@ -7,11 +7,16 @@ set -e -u
 # Run the errand for the appropriate deployment
 #
 
-# Hack: Add trailing newline to skip OTP prompt
 bosh-cli -n -e "${BOSH_TARGET}" --ca-cert "${BOSH_CACERT}" alias-env env
-bosh-cli -e env log-in <<EOF 1>/dev/null
+
+if [ -n "${BOSH_USERNAME:-}" ]; then
+  # Hack: Add trailing newline to skip OTP prompt
+  bosh-cli -e env log-in <<EOF 1>/dev/null
 ${BOSH_USERNAME}
 ${BOSH_PASSWORD}
+
+EOF
+fi
 
 EOF
 bosh-cli -n -e env -d "${BOSH_DEPLOYMENT_NAME}" run-errand "${BOSH_ERRAND}"
