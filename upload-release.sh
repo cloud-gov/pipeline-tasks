b9a8e3f8c552da19fe38017e039a7dcd14ec422b
Joshua Carp <jm.carp@gmail.com>
2017-01-23 18:45:49 -0500

    Log in with client credentials if passed. (#39)

--- upload-release.sh
+++ upload-release.sh
@@ -2,19 +2,21 @@
 
 set -e -u
 
-# Hack: Add trailing newline to skip OTP prompt
-
 BOSH_CERT="${BOSH_CERT:-}"
 if [ -n "${BOSH_CERT}" ]; then
   bosh-cli -n -e "${BOSH_TARGET}" --ca-cert "certificate/${BOSH_CERT}" alias-env env
 else
   bosh-cli -n -e "${BOSH_TARGET}" alias-env env
 fi
-bosh-cli -e env log-in <<EOF 1>/dev/null
+
+if [ -n "${BOSH_USERNAME:-}" ]; then
+  # Hack: Add trailing newline to skip OTP prompt
+  bosh-cli -e env log-in <<EOF 1>/dev/null
 ${BOSH_USERNAME}
 ${BOSH_PASSWORD}
 
 EOF
+fi
 
 for r in release/*.tgz; do
   bosh-cli -e env upload-release "${r}"
