c8238b8863569c25bf714088c43b6c6c5c84776d
Mike Lloyd <mike.lloyd@gsa.gov>
2020-01-14 12:33:04 -0700

    fixing the errand reference.
    Signed-off-by: Mike Lloyd <mike.lloyd@gsa.gov>

--- bosh-errand.sh
+++ bosh-errand.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 # vim: set ft=sh
 
-set -e -u
-
-#
-# Run the errand for the appropriate deployment
-#
-
-bosh-cli -n -e "${BOSH_TARGET}" --ca-cert "${BOSH_CACERT}" alias-env env
-
-if [ -n "${BOSH_USERNAME:-}" ]; then
-  # Hack: Add trailing newline to skip OTP prompt
-  bosh-cli -e env log-in <<EOF 1>/dev/null
-${BOSH_USERNAME}
-${BOSH_PASSWORD}
-
-EOF
-fi
+set -eu
 
 args=("${BOSH_ERRAND}")
 args+=(${BOSH_FLAGS:-})
-bosh-cli -n -e env -d "${BOSH_DEPLOYMENT_NAME}" run-errand "${args[@]}"
+bosh -n run-errand "${args[@]}"
