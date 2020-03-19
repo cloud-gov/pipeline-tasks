eb9f27d4ab10eb87648a40fedbe65de0da5c395f
Ben Berry <ben.berry@gsa.gov>
2020-03-19 09:19:40 -0700

    more variable name corrections

--- sync-bucket.sh
+++ sync-bucket.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -eux
 
-export AWS_DEFAULT_REGION=${DESTINATION_REGION}
-aws s3 sync --source-region=${SOURCE_REGION} \
+export AWS_DEFAULT_REGION=${DESTINATION_BUCKET_REGION}
+aws s3 sync --source-region=${SOURCE_BUCKET_REGION} \
         --sse=AES256 \
         --metadata-directive=COPY \
         s3://${SOURCE_BUCKET}/${SUBDIRECTORY} \
