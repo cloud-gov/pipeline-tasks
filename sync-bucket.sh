aa43e376c5b79cc8ae15940532ec94a4a2961573
Ben Berry <ben.berry@gsa.gov>
2020-03-19 09:10:11 -0700

    consistenter variable names

--- sync-bucket.sh
+++ sync-bucket.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -eux
 
-export AWS_DEFAULT_REGION=${DEST_REGION}
+export AWS_DEFAULT_REGION=${DESTINATION_REGION}
 aws s3 sync --source-region=${SOURCE_REGION} \
         --sse=AES256 \
         --metadata-directive=COPY \
         s3://${SOURCE_BUCKET}/${SUBDIRECTORY} \
-        s3://${DEST_BUCKET}/${SUBDIRECTORY}
+        s3://${DESTINATION_BUCKET}/${SUBDIRECTORY}
