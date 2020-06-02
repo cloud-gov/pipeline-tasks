80d383ce51e9dbfc0e534fc24f8a79e3a5230be5
Steve Greenberg <steve.greenberg@gsa.gov>
2020-06-02 09:45:01 -0600

    add the --delete flag to ensure files deleted in source are removed from the destination

--- sync-bucket.sh
+++ sync-bucket.sh
@@ -2,7 +2,8 @@
 set -eux
 
 export AWS_DEFAULT_REGION=${DESTINATION_BUCKET_REGION}
-aws s3 sync --source-region=${SOURCE_BUCKET_REGION} \
+aws s3 sync --delete \
+        --source-region=${SOURCE_BUCKET_REGION} \
         --sse=AES256 \
         --metadata-directive=COPY \
         s3://${SOURCE_BUCKET}/${SUBDIRECTORY} \
